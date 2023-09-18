e59cb223cbb321d851163f2daea8a23f722d3b29
Güneş Gümüş <22320608+gunesgumus@users.noreply.github.com>
2023-09-18 18:36:33 +0300

    Imported TextMeshPro. Created a scene for testing configurable joints as wheels. After increasing the rigidbody's max angular speed with a script, they work well.

---
+++ Wheeled Armed Hybrid/Assets/Scripts/Physics/MaxAngularSpeedSetter.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class MaxAngularSpeedSetter : MonoBehaviour
+{
+	[SerializeField]
+	float maxAngularSpeed = 50.0f;
+
+	private void Awake()
+	{
+		this.GetComponent<Rigidbody>().maxAngularVelocity = maxAngularSpeed;
+	}
+}
